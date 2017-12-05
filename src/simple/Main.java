09d6b384422047b3a1e5f53196abd4d270877dc4
Carlos Vega González <carlosvegon@gmail.com>
2017-12-05 11:05:47 +0100

    Version2

---
+++ src/simple/Main.java
@@ -0,0 +1,44 @@
+package simple;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        this.stage=primaryStage;
+        mainWindow();
+    }
+
+    public void mainWindow(){
+        try {
+
+            FXMLLoader loader=new FXMLLoader(Main.class.getResource("Ejemplo.fxml"));
+            AnchorPane pane=loader.load();
+            Scene scene=new Scene(pane);
+        scene.getStylesheets().addAll(Main.class.getResource("style.css").toExternalForm());
+        
+        stage.setTitle("signin");
+        stage.setScene(scene);
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
